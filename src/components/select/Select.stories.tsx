2aa06114336b02a3c39315d12bb3bdeec92c0871
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-07 21:55:07 +0300

    Stories for Select added

---
+++ src/components/select/Select.stories.tsx
@@ -0,0 +1,21 @@
+import {Select} from './Select.tsx';
+
+export default {
+    title: 'Select',
+}
+
+export const UnCollapsedAccordion = () => {
+    return (
+        <Select
+            titleValue={'Users'}
+            value={'Users'}
+            items={[
+                {title: 'Dimych', value: 1},
+                {title: 'Valera', value: 2},
+                {title: 'Viktor', value: 3},
+                {title: 'Igor', value: 4},
+            ]}
+            onChange={() => {}}
+        />
+    )
+}
