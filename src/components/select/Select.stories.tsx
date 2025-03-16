0b838e0599b941f2544cd68adc387507bc986298
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-16 22:22:18 +0300

    Stories for New Select added

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -1,20 +1,41 @@
 import {Select} from './Select.tsx';
+import {useState} from 'react';
 
 export default {
     title: 'Select',
 }
 
-export const UnCollapsedAccordion = () => {
+export const WithValue = () => {
+    const [value, setValue] = useState('2');
+
     return (
         <Select
+            onChange={setValue}
+            value={value}
             titleValue={'Users'}
             items={[
-                {title: 'Dimych', value: 1},
-                {title: 'Valera', value: 2},
-                {title: 'Viktor', value: 3},
-                {title: 'Igor', value: 4},
+                {value: '1', title: 'Minsk'},
+                {value: '2', title: 'Moscow'},
+                {value: '3', title: 'Kiev'},
+                {value: '4', title: 'Beijing'},
+            ]}/>
+    )
+}
+
+export const WithoutValue = () => {
+    const [value, setValue] = useState(null);
+
+    return (
+        <Select
+            onChange={setValue}
+            titleValue={'Users'}
+            value={value}
+            items={[
+                {value: '1', title: 'Minsk'},
+                {value: '2', title: 'Moscow'},
+                {value: '3', title: 'Kiev'},
+                {value: '4', title: 'Beijing'},
             ]}
-            onChange={() => {}}
         />
     )
 }
