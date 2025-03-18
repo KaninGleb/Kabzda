f97753a4359582d53da6f17524d2fbb7cc827ec9
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-18 23:09:40 +0300

    UncontrolledAccordion - reducer added

--- src/components/unControlledAccordion/UncontrolledAccordion.tsx
+++ src/components/unControlledAccordion/UncontrolledAccordion.tsx
@@ -1,5 +1,5 @@
 import React, {useReducer} from 'react';
-import {reducer, TOGGLE_CONSTANT} from './Reducer.tsx';
+import {reducer, TOGGLE_CONSTANT} from './reducer.tsx';
 
 type AccordionPropsType = {
     title: string
