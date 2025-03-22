d3a100fc17df5b77d8dd425016a429b73c71a5a6
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-22 21:44:07 +0300

    All Storybook's errors fixed

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,15 +2,15 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: [
-    "../src/**/*.mdx",
+    // "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
     '@storybook/addon-actions',
+    "@storybook/experimental-addon-test",
     '@storybook/react-vite',
   ],
   "framework": {
