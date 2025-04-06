c83e7295ff9917c86967ca4eea4fe268f55f68a0
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-06 16:11:08 +0300

    UseEffect SimpleExample story added

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,43 @@
+import {useEffect, useState} from 'react';
+
+
+export default {
+  title: 'useEffect demo',
+}
+
+
+export const SimpleExample = () => {
+  const [fake, setFake] = useState(1);
+  const [counter, setCounter] = useState(1);
+
+  console.log('SimpleExample');
+
+  // useEffect(() => {
+  //   console.log('useEffect');
+  //
+  //   const title = document.getElementById('title');
+  //   if (title) title.textContent = counter.toString();
+  //
+  // }, [counter]);
+
+  useEffect(() => {
+    console.log('1. useEffect every render');
+  });
+
+  useEffect(() => {
+    console.log('2. useEffect only first render (componentDidMount)');
+  }, []);
+
+  useEffect(() => {
+    console.log('3. useEffect first render and every counter changing');
+  }, [counter]);
+
+  return (
+    <>
+      <div id={'title'}></div>
+      Hello, {fake} {counter}
+      <button onClick={() => setFake(fake + 1)}>fake +</button>
+      <button onClick={() => setCounter(counter + 1)}>counter +</button>
+    </>
+  )
+}
