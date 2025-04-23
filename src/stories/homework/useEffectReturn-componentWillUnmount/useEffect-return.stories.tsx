a131afc80eb8b817da16cbf1d519bb5ed773f42e
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-23 16:28:33 +0300

    SetTimeoutExampleHW1 added

--- src/stories/homework/useEffectReturn-componentWillUnmount/useEffect-return.stories.tsx
+++ src/stories/homework/useEffectReturn-componentWillUnmount/useEffect-return.stories.tsx
@@ -26,3 +26,27 @@ export const SetTimeoutExampleHW1RendersMistake = () => {
   )
 }
 
+export const SetTimeoutExampleHW1 = () => {
+  const [count, setCount] = useState(0);
+
+  console.log('SetTimeoutExample rendered')
+
+  useEffect(() => {
+    const button = document.getElementById('b1');
+    const handleClick = () => setCount((prevState) => prevState + 1);
+
+    button?.addEventListener('click', handleClick)
+    console.log('Effect occurred')
+
+    return () => {
+      button?.removeEventListener('click', handleClick)
+      console.log('Cleanup occurred');
+    }
+  }, []);
+
+  return (
+    <>
+      Count - {count} <button id={'b1'}>+</button>
+    </>
+  )
+}
